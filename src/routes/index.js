790cdd6fc7298bd62beea0ae120b8a7152098b8f
KieuLong-UET <kieunhatlong@gmail.com>
2022-05-27 02:02:12 +0700

    config router

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
+import Home from '~/pages/Home';
+import Shop from '~/pages/Shop';
+import Order from '~/pages/Order';
+
+//Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/shop', component: Shop },
+    { path: '/order', component: Order, layout: HeaderOnly },
+];
+
+//Private routes
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };

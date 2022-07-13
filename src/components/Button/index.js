ebf486d75f31b40daa56caafd3359ffd881e2a8b
KieuLong-UET <kieunhatlong@gmail.com>
2022-07-14 00:15:02 +0700

    Code button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button(to, href, primary, children) {
+    let Comp = 'button';
+
+    return <Comp></Comp>;
+}
+
+export default Button;
